db2ff99b55f408b43f82ec9187434cb0eee521f5
SeobisBack <seobisback@gmail.com>
2022-01-13 11:24:47 +0900

    calc

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var router = require('./router/index');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var session = require('express-session');
+var flash = require('connect-flash');
 
 app.listen(19991, function() {
    console.log("Start! express server on port 19991");
@@ -11,5 +15,15 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true})); // encoding 된 데이터 처리(아스키코드 형태의 데이터만 받을 수 있는데 다른 문자열로 치환)
 app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
 
 app.use(router); // path가 없을 때 '/' 생략 가능.
--- router/join/index.js
+++ router/join/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var mysql = require('mysql');
+const passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 var path = require('path');
 
 //DataBase Setting
@@ -17,20 +19,31 @@ connection.connect();
 
 //Router
 router.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, '../../public/join.html'));
+    res.render('join.ejs');
 });
 
-router.post('/', function(req,res){
-    var body = req.body;
-    var email = body.email;
-    var name = body.name;
-    var passwd = body.password;
+passport.use('local-join', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback : true
+    }, function(req, email, password, done){
+        console.log('local-join callback called');
+}));
 
-    var query = connection.query(`INSERT INTO user (name, email, password) VALUES ("${name}", "${email}", "${passwd}")`, function(err, rows){
-        if (err) throw err;
-        console.log('create user');
-    });
+// router.post('/', function(req,res){
+//     var body = req.body;
+//     var email = body.email;
+//     var name = body.name;
+//     var passwd = body.password;
+
+//     var sql = {name: name, email: email, password: passwd};
+
+//     var query = connection.query(`INSERT INTO user set ?`, sql, function(err, rows){
+//         if (err) throw err;
+//         console.log(`Create the User ID : ${rows.insertId}, Name : ${name} `);
+//         res.render('welcome.ejs', {'name': name, 'id': rows.insertId});
+//     });
     
-});
+// });
 
 module.exports = router;
